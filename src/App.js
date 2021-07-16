795006e06cb16704082db79b57e5589436c63967
Alessia Ruocco <83032347+Aruoc355@users.noreply.github.com>
2021-07-16 13:29:30 +0000

    added host and link to repository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/alessia-ruocco-17a61041/"
+          target="_blank"
+        >
+          Alessia Ruocco
+        </a>{" "}
+        , open-sourced on{" "}
+        <a href="https://github.com/Aruoc355/react-weather-app" target="_blank">
+          GitHub
+        </a>
+        and hosted on{" "}
+        <a href="https://cranky-booth-38afee.netlify.app/" target="_blank">
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
