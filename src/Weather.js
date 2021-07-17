bd2847282709139790c4519d16b66f8a751c118a
Alessia Ruocco <83032347+Aruoc355@users.noreply.github.com>
2021-07-17 17:58:32 +0000

    added property to weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-export default function Weather() {
+
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
@@ -71,7 +72,7 @@ export default function Weather() {
   } else {
     const apiKey = "86795cd1e54d46ec82adad776beccce9";
     let city = "London";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading";
